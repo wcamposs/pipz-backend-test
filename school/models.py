546b54d4eb8b6d36fd18b60d4ed9dfa9e8ae495e
Willian <williancampos1995@gmail.com>
2019-06-08 12:05:09 -0300

    First version

--- school/models.py
+++ school/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Teacher(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.TextField()
+    name = models.CharField(max_length=120)
 
     def __str__(self):
         return self.name
@@ -11,7 +11,7 @@ class Teacher(models.Model):
 
 class Student(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.TextField()
+    name = models.CharField(max_length=120)
     teacher = models.ForeignKey(Teacher, related_name='students', on_delete=models.CASCADE)
 
     def __str__(self):
